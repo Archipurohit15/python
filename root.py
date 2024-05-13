ef44682f669ec739851965434ef4fdccdfbd4acd
Archipurohit15 <162157142+Archipurohit15@users.noreply.github.com>
2024-05-13 12:41:57 +0530

    Add files via upload

---
+++ root.py
@@ -0,0 +1,9 @@
+import math
+
+# Input a number
+num = float(input("Enter a number: "))
+
+# Find square root using math.sqrt()
+square_root = math.sqrt(num)
+
+print("Square root of", num, "is", square_root)
