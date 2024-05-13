f4c2598b1bd3e3119245e166da2c641ca59e0cc2
Archipurohit15 <162157142+Archipurohit15@users.noreply.github.com>
2024-05-13 12:55:26 +0530

    Add files via upload

---
+++ int to float.py
@@ -0,0 +1,8 @@
+integer_value = 10
+
+# Convert integer to float
+float_value = float(integer_value)
+
+# Print the converted value
+print("Integer value:", integer_value)
+print("Float value:", float_value)
